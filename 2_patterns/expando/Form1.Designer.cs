37fab55f8021e9671ed31d5659659e70cbe3f13b
pipiscrew <pipiscrew@gmail.com>
2019-03-07 21:06:06 +0200

    Add files via upload

---
+++ 2_patterns/expando/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dg = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "execute with params";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "IEnumerable<Bike>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(13, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(224, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "dynamic";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(13, 268);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(224, 42);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Datatable";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dg
+            // 
+            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg.Location = new System.Drawing.Point(13, 329);
+            this.dg.Name = "dg";
+            this.dg.Size = new System.Drawing.Size(224, 150);
+            this.dg.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(12, 166);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(224, 42);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "ExecuteScalarQuery";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(13, 217);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(224, 42);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "IDataRecord";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 511);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dg);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dg;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ 2_patterns/expando/SQLClass.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Dynamic;
+
+namespace WindowsFormsApp1
+{
+    public class SQLClass
+    {
+
+        private SqlConnection objConn;
+        private string m_ConnectionString;
+
+        public SQLClass(string ConnectionString, out SqlException ExceptionObject)
+        {
+            try
+            {
+                m_ConnectionString = ConnectionString;
+                objConn = new SqlConnection(ConnectionString);
+                objConn.Open();
+
+                ExceptionObject = null;
+            }
+            catch (SqlException ex)
+            {
+                objConn = null;
+                ExceptionObject = ex;
+            }
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                if (objConn == null | objConn.State != ConnectionState.Open)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public string ConnectionString
+        {
+            get { return m_ConnectionString; }
+        }
+
+        public SqlDataAdapter GetAdapter(string sql)
+        {
+            return new SqlDataAdapter(sql, objConn);
+        }
+
+        public SqlCommand GetCommand(string Query)
+        {
+            return new SqlCommand(Query, objConn);
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return objConn;
+        }
+
+        public IEnumerable<ExpandoObject> ExecuteReadQuery(string query)
+        {
+            using (var command = new SqlCommand(query, objConn))
+            {
+                using (var rdr = command.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        var row = new ExpandoObject() as IDictionary<string, object>;
+                        for (var fieldCount = 0; fieldCount < rdr.FieldCount; fieldCount++)
+                        {
+                            row.Add(rdr.GetName(fieldCount), rdr[fieldCount]);
+                        }
+                        yield return (ExpandoObject)row;
+                    }
+                }
+            }
+        }
+
+
+        public int ExecuteUpdateQuery(string query)
+        {
+            int rows = -1;
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, objConn))
+                {
+                    rows = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return rows;
+        }
+
+
+        public int ExecuteUpdateQuery(string query, Dictionary<string, string> parameters)
+        { //https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlparametercollection.addwithvalue
+            int rows = -1;
+            try
+            {
+                    using (SqlCommand cmd = new SqlCommand(query, objConn))
+                    {
+                        foreach (KeyValuePair<string, string> field in parameters)
+                        {
+                            cmd.Parameters.AddWithValue(field.Key, field.Value);
+                        }
+
+                        rows = cmd.ExecuteNonQuery();
+                    }                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return rows;
+        }
+
+
+        public object ExecuteScalarQuery(string query)
+        {
+            object result = null;
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand(query, objConn))
+                {
+                    result = cmd.ExecuteScalar();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return result;
+        }
+
+        public DataTable GetDataTable(string query)
+        {
+            SqlDataAdapter sqlAD = new SqlDataAdapter();
+            DataTable sqlSET = new DataTable();
+            SqlCommand sqlco = new SqlCommand();
+
+            try
+            {
+                sqlco.CommandText = query;
+                sqlco.Connection = objConn;
+
+                sqlAD.SelectCommand = sqlco;
+                //sqlAD.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+                sqlAD.Fill(sqlSET);
+
+                return sqlSET;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sqlco.Dispose();
+                sqlAD.Dispose();
+                sqlSET.Dispose();
+            }
+        }
+
+        public DataSet GetDataSet(string query)
+        {
+            SqlDataAdapter sqlAD = new SqlDataAdapter();
+            DataSet sqlSET = new DataSet();
+            SqlCommand sqlco = new SqlCommand();
+
+            try
+            {
+                sqlco.CommandText = query;
+                sqlco.Connection = objConn;
+
+                sqlAD.SelectCommand = sqlco;
+                sqlAD.Fill(sqlSET, "tabl");
+                return sqlSET;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                sqlco.Dispose();
+                sqlAD.Dispose();
+                sqlSET.Dispose();
+            }
+        }
+    }
+}
